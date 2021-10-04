33b0d4d06f07602920688cd6a8ea804306da34db
geekyVaibhav-07 <70719087+geekyVaibhav-07@users.noreply.github.com>
2021-10-04 08:05:38 +0530

    feat: add loader (#3)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
-import { Loop } from '@mui/icons-material';
-import { Button } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { useSelector, useDispatch } from 'react-redux';
 import CssBaseline from '@mui/material/CssBaseline';
 import { getAuthStatus } from './store/appAction';
+import Loader from './components/loader/loader';
 import './App.css';
 
 const  App = () => {
@@ -21,15 +20,16 @@ const  App = () => {
     }, []);
 
     const loadingScreen = () => {
-        return <Loop className="App-loader" color="primary" sx={{ fontSize: 200 }}/>;
+        return (
+            <Loader open={authenticationStatus !== 'UNKNOWN'} />
+        )
     }
 
     return (
         <ThemeProvider  theme={theme}>
             <CssBaseline />
             <div className="App">
-                {authenticationStatus === 'UNKNOWN' ? 'Login' : loadingScreen()}
-                <Button variant="outlined">Outlined</Button>
+                {loadingScreen()}
             </div>
         </ThemeProvider>
         
---
+++ src/components/loader/loader.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Modal } from '@mui/material';
+import { Loop } from '@mui/icons-material';
+
+const defaultWrapperStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    outline: 'none'
+};
+
+const defaultStyle = {
+    fontSize: '200px',
+    animation: 'spinner infinite 2s linear',
+    outline: 'none'
+}
+
+const Loader = (props) => {
+    const {
+        wrapperStyleOveride = {},
+        styleOverride = {},
+        handleClose,
+        open = false,
+    }  = props;
+
+    const wrapperStyle = {
+        ...defaultWrapperStyle,
+        ...wrapperStyleOveride
+    };
+
+    const style = {
+        ...defaultStyle,
+        ...styleOverride
+    };
+
+    return (
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <div style={wrapperStyle}>
+                <Loop color="primary" sx={style} />
+            </div>
+        </Modal>
+    )
+};
+
+export default Loader
